c1b3d81a9ca958b9b54c32c26ab05734d26f495b
davidkethel <davidkethel@gmail.com>
2013-09-27 15:19:52 +1000

    Added MVC Web Api

--- RandomPush/Global.asax.cs
+++ RandomPush/Global.asax.cs
@@ -2,19 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
 using Microsoft.AspNet.SignalR;
+
 
 namespace RandomPush
 {
     public class Global : System.Web.HttpApplication
     {
 
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("{resource}.aspx/{*pathInfo}");
+
+            routes.MapRoute(
+                "Default", // Route name
+                "{controller}/{action}/{id}", // URL with parameters
+                new { controller = "Assets", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+                );
+        }
+        
         protected void Application_Start(object sender, EventArgs e)
         {
             RouteTable.Routes.MapHubs();
+            RegisterRoutes(RouteTable.Routes);
+
         }
 
         protected void Session_Start(object sender, EventArgs e)
