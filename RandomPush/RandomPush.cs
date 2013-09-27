40930d9c11e6ee969a050b4156a5b8bf539d3d96
davidkethel <davidkethel@gmail.com>
2013-09-27 14:55:15 +1000

    Finshed Basic Random

---
+++ RandomPush/RandomPush.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace RandomPush
+{
+    public class RandomPush : Hub
+    {
+        
+        public void Push()
+        {
+            while (true)
+            {
+                Clients.All.Send("Random Push");
+
+                var rand = new Random();
+                Thread.Sleep(rand.Next(0, 1000));
+            }
+        }
+    }
+}
