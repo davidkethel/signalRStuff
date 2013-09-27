264af170e513281d688a8c3a013cf99b15c03f1d
davidkethel <davidkethel@gmail.com>
2013-09-27 15:31:54 +1000

    Clean Up

--- RandomPush/Global.asax.cs
+++ RandomPush/Global.asax.cs
@@ -1,67 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
-using System.Web.Mvc;
 using System.Web.Routing;
-using System.Web.Security;
-using System.Web.SessionState;
-using Microsoft.AspNet.SignalR;
-
 
 namespace RandomPush
 {
-    public class Global : System.Web.HttpApplication
+    public class Global : HttpApplication
     {
-
-
-        public static void RegisterRoutes(RouteCollection routes)
-        {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-            routes.IgnoreRoute("{resource}.aspx/{*pathInfo}");
-
-            routes.MapRoute(
-                "Default", // Route name
-                "{controller}/{action}/{id}", // URL with parameters
-                new { controller = "Assets", action = "Index", id = UrlParameter.Optional } // Parameter defaults
-                );
-        }
-        
         protected void Application_Start(object sender, EventArgs e)
         {
             RouteTable.Routes.MapHubs();
-            RegisterRoutes(RouteTable.Routes);
-
         }
 
         protected void Session_Start(object sender, EventArgs e)
         {
-
         }
 
         protected void Application_BeginRequest(object sender, EventArgs e)
         {
-
         }
 
         protected void Application_AuthenticateRequest(object sender, EventArgs e)
         {
-
         }
 
         protected void Application_Error(object sender, EventArgs e)
         {
-
         }
 
         protected void Session_End(object sender, EventArgs e)
         {
-
         }
 
         protected void Application_End(object sender, EventArgs e)
         {
-
         }
     }
 }
--- RandomPush/RandomPush.cs
+++ RandomPush/RandomPush.cs
@@ -1,15 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
-using System.Web;
 using Microsoft.AspNet.SignalR;
 
 namespace RandomPush
 {
     public class RandomPush : Hub
     {
-        
         public void Push()
         {
             while (true)
