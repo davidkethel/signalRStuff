487d6e21274539a672d8ddafb2236ed98e994dac
davidkethel <davidkethel@gmail.com>
2013-09-27 16:31:09 +1000

    Added Random Controller and SignalR to WebApiSignalR

---
+++ WebApiSignalR/Controllers/RandomController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace WebApiSignalR.Controllers
+{
+    public class RandomController : ApiController
+    {
+        // POST api/Random
+        public void Post()
+        {
+            var i = 0;
+        }
+
+        // POST api/Delete
+        public void Delete()
+        {
+            var i = 0;
+        }
+    }
+}
