6562f2c48b3af1496f2c8ae9529e1f4ad6cb9cb8
davidkethel <davidkethel@gmail.com>
2013-09-30 15:47:38 +1000

    Finished Web Api and Signal R

--- WebApiSignalR/Controllers/RandomController.cs
+++ WebApiSignalR/Controllers/RandomController.cs
@@ -1,19 +1,40 @@
-﻿namespace WebApiSignalR.Controllers
+﻿using System;
+using System.Threading;
+
+namespace WebApiSignalR.Controllers
 {
     public class RandomController : APIControllerWithHub<RandomHub>
     {
+        private static bool _randomStarted;
+        private static bool _randomOn;
+
         // POST api/Random
         public void Post()
         {
-            var i = 0;
-
-            Hub.Clients.All.push("Hello Push");
+            if (!_randomStarted)
+            {
+                _randomOn = true;
+                StartRandom();
+            }
         }
 
         // POST api/Delete
         public void Delete()
         {
-            var i = 0;
+            _randomOn = false;
+        }
+
+        private void StartRandom()
+        {
+            _randomStarted = true;
+
+            while (_randomOn)
+            {
+                Hub.Clients.All.push("Random");
+
+                var rand = new Random();
+                Thread.Sleep(rand.Next(0, 1000));
+            }
         }
     }
 }
